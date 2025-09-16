c6228b4ae8b171cea53e80b5de290102a8d006ce
Moniter5316 <139362710+Moniter5316@users.noreply.github.com>
2025-09-17 02:04:11 +0700

    cell03

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,7 @@
+mes = input("What you gotta say? : ")
+while True :
+    if mes == "STOP" :
+        break
+    else :
+        mes = input("I got that! Anything else? : ")
+        
---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,5 @@
+for i in range(11) :
+    print(f"Table de {i}: ",end="")
+    for y in range(11):
+        print(i*y," ",end="")
+    print()    
