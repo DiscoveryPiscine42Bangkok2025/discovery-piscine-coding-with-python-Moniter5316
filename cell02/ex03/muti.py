b6c8a231d4693831e2f7dc885fb1fd253bd4d12d
MoniterZ <139362710+Moniter5316@users.noreply.github.com>
2025-09-17 13:23:52 +0700

    Update muti.py

--- cell02/ex03/muti.py
+++ cell02/ex03/muti.py
@@ -2,7 +2,7 @@ y = int(input("Number : "))
 x = int(input("Number : "))
 if y*x == 0 :
     print(y*x)
-    print("This result is both positive and negative")
+    print("This result is positive and negative")
 elif y*x <0 :
     print(y*x)
     print("The result is negative.")
