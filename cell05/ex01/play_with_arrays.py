23a175fdcf5551a3fe5fa659ed42cef6b757aa06
MoniterZ <139362710+Moniter5316@users.noreply.github.com>
2025-09-17 16:46:13 +0700

    update

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,6 @@
+arr = [2, 8, 9, 48, 8, 22,-12, 2]
+arr2 = []
+for i in arr :
+    arr2.append(i+2)
+print("Original array: [2, 8, 9, 48, 8, 22,-12, 2]")
+print("New array:",arr2)
---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,7 @@
+arr = [2, 8, 9, 48, 8, 22,-12, 2]
+new = []
+print(arr)
+for i in arr:
+    if(i > 3):
+        new.append(i+2)
+print(new)
