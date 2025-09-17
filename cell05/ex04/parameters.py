13f105d69d58091abf3c7f1b6bb18fe27f00d8d1
Moniter5316 <139362710+Moniter5316@users.noreply.github.com>
2025-09-17 22:59:05 +0700

    ex05

---
+++ cell05/ex04/parameters.py
@@ -0,0 +1,4 @@
+import sys
+
+params = sys.argv[1:]
+print(f"Number of parameters: {len(params)}.")
---
+++ cell05/ex05/aff_first_param.py
@@ -0,0 +1,7 @@
+import sys 
+param = sys.argv[1:]
+number_param = len(param)
+if number_param == 0 :
+    print("none")
+else :
+    print(param[0])
