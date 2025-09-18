8ee348073b4d7ef8c9d60e389ad3f4cd362ef245
Moniter5316 <139362710+Moniter5316@users.noreply.github.com>
2025-09-18 17:14:45 +0700

    ex12

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -1,13 +1,12 @@
 import sys
-text = sys.argv[1:]
-
-if len(text) == 1 :
+text = sys.argv[1]
+if len(sys.argv[1:]) == 1 :
     
     cont = text.count("z")
 
     if cont == 0 :
-        print(text)
+        print("none")
     else :
         print("z"*cont)
 else :
-    print("none")   
+    print("none")
