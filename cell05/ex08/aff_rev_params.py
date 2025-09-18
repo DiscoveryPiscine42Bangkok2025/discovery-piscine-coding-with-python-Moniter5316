85d390b7c5cf2d10dacc21d3d33c554c69299579
Moniter5316 <139362710+Moniter5316@users.noreply.github.com>
2025-09-18 16:28:13 +0700

    update

--- cell05/ex08/aff_rev_params.py
+++ cell05/ex08/aff_rev_params.py
@@ -1,8 +1,9 @@
 import sys
 
 params = sys.argv[1:]
-if len(params) >=2 :
-    for i in reversed(params) :
+
+if len(params) >= 2:
+    for i in reversed(params):
         print(i)
-else :
+else:
     print("none")
